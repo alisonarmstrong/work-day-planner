2c727dd4ae87b295aeaa93d0800ec91b61596cdc
alisonarmstrong <alarmstrong15@gmail.com>
2021-03-19 09:32:16 -0600

    added time and column sizes

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,13 @@
+var saveBtn = $(".saveBtn");
+var currentHour = moment().format("HH");
+
+
+// current date in the header //
 $("#currentDay").text(moment().format('MMMM Do YYYY'));
+
+// change color in each row //
+
+// local storage //
+saveBtn.on("click", function (){
+    localStorage.setItem(input);
+});
